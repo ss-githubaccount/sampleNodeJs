bf7c71d3d64828ece6aaf3274eeb0db9b473ca53
Stephen Stinson <sstinson@cimpress.com>
2017-04-13 16:26:06 -0400

    Removing hashmark from url in angular routing

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -1,9 +1,13 @@
 angular.module('sampleApp', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
         $routeProvider.when('/', {
             templateUrl: '../partials/test-partial.html',
             controller: 'indexController'
-        })
+        });
     }])
     .controller('indexController', ['$scope', function($scope) {
         console.log("starting controller");
